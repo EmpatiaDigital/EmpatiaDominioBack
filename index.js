1ef1464a6b3a0553c0f4040a170e8aa354ca778c
EmpatiaDigital <empatiadigital2025@gmail.com>
2025-06-19 08:44:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use("/descargas", express.static(path.join(__dirname, "public/descargas")));
 // Rutas
-app.use('/api', testRoutes);
+app.get('/api', (req, res) => {
+  res.send('Ahora sí estoy');
+});
 app.use('/api', changePassword);
 app.use('/api/auth', authRoutes);
 app.use('/api',sociosRoutes);
