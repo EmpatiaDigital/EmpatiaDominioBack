c7bb4209a1f9d6a085cbdd950f48fd1f5139484d
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-02-04 13:40:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const userActividadRoutes = require('./routes/userActividad');
 const path = require("path");
 const Post = require("./models/Post");
 
+
+const courseRoutes = require('./routes/courseRoutes');
+const inscriptionRoutes = require('./routes/inscriptionRoutes');
+
 // Usamos las rutas
 dotenv.config();
 const app = express();
@@ -39,6 +43,10 @@ app.use('/api/descarga', descargaRoutes);
 app.use('/api', userActividadRoutes);
 
 
+
+app.use('/api/courses', courseRoutes);
+app.use('/api/inscriptions', inscriptionRoutes);
+
 // Conexión a la base de datos
 mongoose.connect("mongodb+srv://empatiadigital2025:Gali282016@empatia1.s1i7isu.mongodb.net/?retryWrites=true&w=majority&appName=Empatia1", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado a MongoDB'))
@@ -49,3 +57,4 @@ mongoose.connect("mongodb+srv://empatiadigital2025:Gali282016@empatia1.s1i7isu.m
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
+
