eca524265a4ad5da7930c2144c0f1e44b3599cc7
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-02-06 21:19:28 -0300

    Update inscriptionRoutes.js

--- routes/inscriptionRoutes.js
+++ routes/inscriptionRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const inscripcionController = require('../controllers/inscripcionController');
+const inscripcionController = require('../controllers/inscriptionController');
 
 // Rutas para inscripciones
 router.get('/curso/:cursoId', inscripcionController.getInscripcionesByCurso);
