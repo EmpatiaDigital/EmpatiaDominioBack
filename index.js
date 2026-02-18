68cf2b1fb73e1d6e38a6a477e1c724d6382ad0dd
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-02-18 17:00:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ const inscriptionRoutes = require('./routes/inscriptionRoutes');
 dotenv.config();
 const app = express();
 
-
 // Middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
@@ -56,6 +55,7 @@ mongoose.connect("mongodb+srv://empatiadigital2025:Gali282016@empatia1.s1i7isu.m
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
+
 
 
 
