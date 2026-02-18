b4d2bfacd307aaf12ef1734fd809fc78e65da08c
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-02-17 21:32:18 -0300

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,18 +1,14 @@
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
-const dotenv = require('dotenv');
-dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dnodisza5",
-  api_key: process.env.CLOUDINARY_API_KEY || "713585429952622",
-  api_secret: process.env.CLOUDINARY_API_SECRET || "Xi0UEkmnjB6PllVt2W5871s8pJ0",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-console.log('=== CLOUDINARY CONFIG ===');
-console.log('cloud_name:', cloudinary.config().cloud_name);
-console.log('api_key:', cloudinary.config().api_key);
-console.log('========================');
+console.log('CLOUDINARY CLOUD NAME:', process.env.CLOUDINARY_CLOUD_NAME);
+console.log('CLOUDINARY API KEY:', process.env.CLOUDINARY_API_KEY);
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
