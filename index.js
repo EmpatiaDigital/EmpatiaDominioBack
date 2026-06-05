d149964e286ff1f287eb87f6bc67fd6e6a71276d
EmpatiaDigital <empatiadigital2025@gmail.com>
2026-06-05 13:59:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ const postStatsRoutes = require('./routes/postStatsRoutes');
 dotenv.config();
 const app = express();
 
-
 // Middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
